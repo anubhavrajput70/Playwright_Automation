b290c466f988f52d5cf36d11ddb6bb56d7dbf84a
anubhavrajput193@gmail.com <anubhavrajput193@gmail.com>
2025-08-11 02:24:12 +0530

    basic test

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -1,4 +1,4 @@
-const test = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 //browser is fixture which is provided by playwright
 //fixture is a global object which is provided by playwright
@@ -6,11 +6,15 @@ test('browser context playwright test', async ({ browser }) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto('https://www.google.com');
-    await page.waitForTimeout(10000);
+    const title = await page.title();
+    console.log(title);
+    await expect(page).toHaveTitle('Google');
 });
 
 //for default context we can use page fixture
 test('page playwright test', async ({ page }) => {
     await page.goto('https://www.google.com');
-    await page.waitForTimeout(10000);
+    const title = await page.title();
+    console.log(title);
+    await expect(page).toHaveTitle('Google');   
 });
