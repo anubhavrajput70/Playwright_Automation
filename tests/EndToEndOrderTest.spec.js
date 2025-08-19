37f6c80e4d363586d4a4f1d258fd11fd87d15d09
anubhavrajput193@gmail.com <anubhavrajput193@gmail.com>
2025-08-19 18:41:07 +0530

    end to end test

---
+++ tests/EndToEndOrderTest.spec.js
@@ -0,0 +1,52 @@
+import { test, expect } from '@playwright/test';
+
+test('End to End Order Test', async ({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+    const productName = "ZARA COAT 3";
+    const email="anubhavrajput@gmail.com";
+    const products = page.locator(".card-body");
+    await page.locator("#userEmail").fill(email);
+    await page.locator("#userPassword").fill("Qwerty@123");
+    await page.locator("[value='Login']").click();
+    await page.locator(".card-body b").first().waitFor();
+    const titles = await page.locator(".card-body b").allTextContents();
+    console.log(titles);
+    const count =await products.count();
+    for(let i=0;i<count;i++)
+    {
+       const productName1 = await products.nth(i).locator("b").textContent();
+        if(productName1 === productName)
+        {
+            //add product to cart
+            //you can also use the text locator to click on the add to cart button
+            //page.locator(".card-body b").first().click
+            await products.nth(i).locator("text=Add to Cart").click();
+            break;
+        }
+    }
+    await page.locator("[routerlink*='cart']").click();
+    //adding manual waits for the cart page to load
+    await page.locator("div li").first().waitFor(); 
+    const bool=await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+    expect(bool).toBeTruthy();
+    await page.locator("text=Checkout").click();
+    await page.locator("[placeholder*='Country']").pressSequentially("ind",{delay:100});
+    const dropdown = page.locator(".ta-results");
+    await dropdown.waitFor();
+    const optionsCount = await dropdown.locator("button").count();
+    for(let i=0;i<optionsCount;i++)
+    {
+        const optionText = await dropdown.locator("button").nth(i).textContent();
+        if(optionText.trim() === "India")
+        {
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+    expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+    await page.locator(".action__submit").click();
+    expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    console.log("Order Id is "+orderId);
+    await page.pause();
+});
